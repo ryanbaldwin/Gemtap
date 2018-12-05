22c5e1233c7dbd9a8dec5c7332b0fb72242d815e
ryanbaldwin <ryanbaldwin@gmail.com>
2018-12-04 21:37:53 -0500

    rdoc the property clock

--- lib/gemtap/models/property.rb
+++ lib/gemtap/models/property.rb
@@ -1,15 +1,34 @@
 require 'liquid'
 
 module Gemtap
+  #
+  # A Liquid Drop class which represents a model's property
+  #
+  # @author [ryanbaldwin]
+  #
   class Property < Liquid::Drop
-    def name
-      @name
-    end
+    #
+    # The name (symbol) of this property
+    attr_reader :name
 
+    #
+    # The type (Int, String, Bool?, etc) for this
+    #
+    # @return [String] The property type
     def type
       @attrs['type']
     end
 
+    #
+    # The default value specified for this value, if one exists.
+    # If the property is not an Optional, and no default is provided,
+    # then `default_value` will attempt to provide one for you which
+    # is `empty'ish`. For example, a required `Bool` will be assigned
+    # `false`, `Double`s 0, etc.
+    #
+    # @return [string] The provided default value for this property, if
+    # one was specified; otherwise if the property is required a sensible
+    # default will be returned; otherwise nil.
     def default_value
       return @attrs['default'] if @attrs.key?('default')
       return nil if type.end_with? '?'
@@ -17,6 +36,12 @@ module Gemtap
       default_value_for(type.downcase.to_sym)
     end
 
+    #
+    # Initializes this Property with a `Mapping of Mappings` yaml sepc.
+    # @param prop [[String, {}]] Accepts an array consisting of a String
+    # (for the property name), and a hash defining its attributes
+    #
+    # @return [type] [description]
     def initialize(prop)
       @name = prop.first
       @attrs = prop.last
@@ -24,6 +49,11 @@ module Gemtap
 
     private
 
+    #
+    # Given a type, returns an appropriate, minimum viable value
+    # @param type [String] the type (eg. 'Int') of this property
+    #
+    # @return [String] A default value for this property
     def default_value_for(type)
       case type.downcase.to_sym
       when :bool then 'false'
