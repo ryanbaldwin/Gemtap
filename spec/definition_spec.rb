dffe7522779b9894166621e9f572903b18dc1063
ryanbaldwin <ryanbaldwin@gmail.com>
2018-12-09 13:57:11 -0500

    make tests more symantec now that rubocop rule is disabled for tests.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -13,15 +13,16 @@ describe Gemtap::Definition do
   end
 
   it 'has a model name' do
-    expect(@definition.model).to(eq('Customer'))
+    expect(@definition.model).to eq 'Customer'
   end
 
   it 'has properties' do
-    expect(@definition.props.count).to(eq(3))
+    expect(@definition.props.count).to be 3
   end
 
   it 'can load form yaml' do
     pn = Pathname.new(__FILE__).dirname + './definitions/customer.yml'
     @definition = Gemtap::Definition.read(pn.expand_path.cleanpath)
+    expect(@definition).to_not be nil
   end
 end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -11,15 +11,15 @@ describe Gemtap::Property do
     end
 
     it 'has a name' do
-      expect(@prop.name).to(eq('id'))
+      expect(@prop.name).to eq 'id'
     end
 
     it 'has a type' do
-      expect(@prop.type).to(eq('Int'))
+      expect(@prop.type).to eq 'Int'
     end
 
     it 'defaults Int properties to 0' do
-      expect(@prop.default_value).to(eq('0'))
+      expect(@prop.default_value).to eq '0'
     end
   end
 
@@ -28,17 +28,17 @@ describe Gemtap::Property do
       prop = Gemtap::Property.new('name'    => 'id',
                                   'type'    => 'Int',
                                   'default' => '50')
-      expect(prop.default_value).to(eq('50'))
+      expect(prop.default_value).to eq '50'
     end
 
     it 'has nil default value when optional and not provided' do
       prop = create_property_of_type('Int?')
-      expect(prop.default_value).to(be_nil)
+      expect(prop.default_value).to be nil
     end
 
     it 'defaults UInt to 0' do
       prop = create_property_of_type('UInt')
-      expect(prop.default_value).to(eq('0'))
+      expect(prop.default_value).to eq '0'
     end
 
     it 'defaults Character to empty' do
@@ -48,22 +48,22 @@ describe Gemtap::Property do
 
     it 'defaults String properties to empty' do
       prop = create_property_of_type('String')
-      expect(prop.default_value).to(eq('""'))
+      expect(prop.default_value).to eq '""'
     end
 
     it 'defaults Bool to false' do
       prop = create_property_of_type('Bool')
-      expect(prop.default_value).to(eq('false'))
+      expect(prop.default_value).to eq 'false'
     end
 
     it 'defaults double to 0.0' do
       prop = create_property_of_type('Double')
-      expect(prop.default_value).to(eq('0.0'))
+      expect(prop.default_value).to eq '0.0'
     end
 
     it 'defaults CGFloat to 0.0' do
       prop = create_property_of_type('CGFloat')
-      expect(prop.default_value).to(eq('0.0'))
+      expect(prop.default_value).to eq '0.0'
     end
   end
 end
